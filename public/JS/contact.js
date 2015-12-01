19339443415a23f71666f3a28c5e825c65cada00
markroze <markroze@gmail.com>
2015-12-01 16:20:31 +0200

    contact fix and games popup

---
+++ public/JS/contact.js
@@ -0,0 +1,13 @@
+function sendForm(obj) {
+    //console.log(obj)
+    //console.log(obj[1].value)
+    var newContact = {name: obj[0].value, mail: obj[1].value, msg: obj[2].value, timeSent: Date.now()};
+    $.ajax({
+        type: "POST",
+        url: 'api/contact',
+        data: newContact,
+        success: function () {
+            console.log('Added!');
+        }
+    });
+}
--- public/JS/games.js
+++ public/JS/games.js
@@ -2,10 +2,10 @@ function renderGames (gamesJSON) {
     $.getJSON("api/games", function (games) {
         var strHtml = [];
         $.each(games, function (key, value) {
-            strHtml.push("<div style='background-image: url(" + value.pic + "); background-size: contain;' onclick='renderDesc(" + key + ")' class='container container-" + key + "'>" +
-                "<div class='child child-" + key + "'></div>" +
-                "<div class='child child-" + key + "'>" + value.name + "</div>" +
+            strHtml.push("<div style='background-image: url(" + value.pic + "); background-size: contain;' onclick='renderDesc(id)' id="+ value.id +" class='container container-" + key + "'>" +
                 "<div class='child child-" + key + "'>" + value.rate + "</div>" +
+                "<div class='child child-" + key + "'>" + value.name + "</div>" +
+                "<div class='child child-" + key + " description'>" + value.desc + "</div>" +
                 "</div>");
         });
 
@@ -17,9 +17,28 @@ function renderGames (gamesJSON) {
 $(document).ready(renderGames);
 
 
-//function renderDesc(key) {
-//
-//}
+function renderDesc(id) {
+    var elDesc = $( '.descriptionPop' );
+    $.getJSON("api/games/"+id, function(game){
+        //console.log(game)
+        $(elDesc).html( '<div class="desc-title">' + game.name + '</div>' +
+                        '<div class="desc-pic"><img src="' + game.pic +'"/></div>' +
+                        '<div class="desc-info">' + game.desc + '</div>' +
+                        '<div class="desc-rating">' + game.rate + '</div>' +
+                        '<div class="desc-raters">' + game.raters + '</div>'
+                                
+        );
+    });
+
+    elDesc.addClass('showDesc');
+    document.querySelector('.cinemaOff').classList.add('cinemaOn');
+
+}
+function removeDesc(key) {
+    document.querySelector('.descriptionPop').classList.remove('showDesc');
+    document.querySelector('.cinemaOff').classList.remove('cinemaOn');
+
+}
 
 //style="background-image: url(
 //    <img src='" + value.pic + "'/>
